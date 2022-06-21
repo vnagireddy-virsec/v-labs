0e45ce5eba075e2045cfffa98e561f7068ff9d2d
Venkata Nagireddy <vnagireddy@gmail.com>
2022-06-21 02:04:11 +0000

    - Generated kafka template project by spring cli.

---
+++ v-spring-kafka-consumer/src/test/java/com/virsec/labs/kafka/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virsec.labs.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
