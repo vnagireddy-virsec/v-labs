99085d15ec699a65d81b9cc60c4e92b17f098ea2
Venkata Nagireddy <vnagireddy@gmail.com>
2022-06-21 06:15:29 +0000

    - Updated kafka consumer to listen and read from topic1.

--- v-spring-kafka-consumer/src/main/java/com/virsec/labs/kafka/ConsumerApplication.java
+++ v-spring-kafka-consumer/src/main/java/com/virsec/labs/kafka/ConsumerApplication.java
@@ -2,37 +2,53 @@ package com.virsec.labs.kafka;
 
 import java.util.Arrays;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
 
 
 @SpringBootApplication
 public class ConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
 
-			System.out.println("================================================================================");
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-			System.out.println("--------------------------------------------------------------------------------");
+            System.out.println("================================================================================");
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            System.out.println("--------------------------------------------------------------------------------");
 
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println("-- " + beanName);
-			}
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println("-- " + beanName);
+            }
 
-			System.out.println("--------------------------------------------------------------------------------");
+            System.out.println("--------------------------------------------------------------------------------");
 
-		};
-	}
+        };
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @KafkaListener(id = "myId", topics = "topic1")
+    public void listen(String in) {
+        System.out.println(in);
+    }
 
 }
