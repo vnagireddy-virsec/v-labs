b5072c60c1f3392c93bf698bc2e6a76d01633207
Venkata Nagireddy <vnagireddy@gmail.com>
2022-06-21 05:24:34 +0000

    - Added skeleton for kafka producer using spring boot.

---
+++ v-spring-kafka-producer/src/main/java/com/virsec/labs/kafka/ProducerApplication.java
@@ -0,0 +1,13 @@
+package com.virsec.labs.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+}
